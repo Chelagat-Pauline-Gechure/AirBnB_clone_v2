216cb3a5e6d97bd9e0a414e5c43b568eddde7ba4
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-10-06 01:58:53 +0300

    Advanced

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 """ Cleans old files"""
 
-from fabric.api import *
-import os
 from datetime import datetime
 import tarfile
+from fabric.api import *
+import os
 
-env.hosts = ["34.73.8.171", "34.74.18.52"]
+env.hosts = ["18.210.18.197 ", "54.86.230.242"]
 env.user = "ubuntu"
 
 
-def do_clean(number=0):
-    """ Removes all but given number of archives"""
-    number = int(number)
-    if number < 2:
-        number = 1
-    number += 1
-    number = str(number)
+def do_clean(value=0):
+    """ Removes all but given value of archives"""
+    value = int(value)
+    if value < 2:
+        value = 1
+    value += 1
+    value = str(value)
     with lcd("versions"):
         local("ls -1t | grep web_static_.*\.tgz | tail -n +" +
-              number + " | xargs -I {} rm -- {}")
+              value + " | xargs -I {} rm -- {}")
     with cd("/data/web_static/releases"):
         run("ls -1t | grep web_static_ | tail -n +" +
-            number + " | xargs -I {} rm -rf -- {}")
+            value + " | xargs -I {} rm -rf -- {}")
