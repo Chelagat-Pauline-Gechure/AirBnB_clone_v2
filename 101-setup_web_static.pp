d60c2c4a83afdab5e544da5532cf9a904e2da592
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-10-06 01:40:51 +0300

    Advanced

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -25,31 +25,6 @@ package { 'nginx':
   provider => 'apt'
 }
 
--> file { '/data':
-  ensure  => 'directory'
-}
-
--> file { '/data/web_static':
-  ensure => 'directory'
-}
-
--> file { '/data/web_static/releases':
-  ensure => 'directory'
-}
-
--> file { '/data/web_static/releases/test':
-  ensure => 'directory'
-}
-
--> file { '/data/web_static/shared':
-  ensure => 'directory'
-}
-
--> file { '/data/web_static/releases/test/index.html':
-  ensure  => 'present',
-  content => "this webpage is found in data/web_static/releases/test/index.htm \n"
-}
-
 -> file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test'
@@ -84,4 +59,28 @@ file { '/var/www':
 
 -> exec { 'nginx restart':
   path => '/etc/init.d/'
+}
+-> file { '/data':
+  ensure  => 'directory'
+}
+
+-> file { '/data/web_static':
+  ensure => 'directory'
+}
+
+-> file { '/data/web_static/releases':
+  ensure => 'directory'
+}
+
+-> file { '/data/web_static/releases/test':
+  ensure => 'directory'
+}
+
+-> file { '/data/web_static/shared':
+  ensure => 'directory'
+}
+
+-> file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "this webpage is found in data/web_static/releases/test/index.htm \n"
 }
